e8b4e63be72caa4f2f701523daf559275154f265
Gustavo Santos <gustavo@after.sale>
2020-09-15 20:16:19 -0300

    added tests and build scripts

---
+++ packages/webapp/src/App.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importDefault(require("react"));
+function App() {
+    return (<div>
+      <h1>Hello!</h1>
+    </div>);
+}
+exports.default = App;
