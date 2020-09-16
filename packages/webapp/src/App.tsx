61bf326619dfd71283675827f1843f2accefb1a7
Gustavo Santos <gustavo@after.sale>
2020-09-15 21:02:53 -0300

    use add function in webapp

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { add } from "@demo/math/lib";
 
 export default function App() {
+  const [x, setX] = useState<number>(0);
+  const [y, setY] = useState<number>(0);
+  const [res, setRes] = useState<number | undefined>();
+
+  const handleCalculate = () => {
+    setRes(add(x, y));
+  };
+
   return (
     <div>
       <h1>Hello!</h1>
+
+      <input
+        type="number"
+        value={x}
+        onChange={(ev) => setX(Number(ev.target.value))}
+      />
+      <input
+        type="number"
+        value={y}
+        onChange={(ev) => setY(Number(ev.target.value))}
+      />
+      <button onClick={handleCalculate}>add</button>
+      <p>add: {res}</p>
     </div>
   );
 }
